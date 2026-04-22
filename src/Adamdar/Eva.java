e567fc7afba7a3d9a1c081664b9dffc7d0e60b23
fedya <fedor.hill69@gmail.com>
2026-04-23 02:26:04 +0500

    update

--- src/Adamdar/Eva.java
+++ src/Adamdar/Eva.java
@@ -8,12 +8,19 @@ import Enums.GENDER;
 /**
  * @author Meiramkhan Alinur
  */
-public class Eva {
+public abstract class Eva implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     /**
      * Default constructor
      */
-    public Eva() {
+    public Eva(String id, String name, String email, LocalDate birthday, GENDER gender, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.password = password;
     }
 
     /**
@@ -41,4 +48,29 @@ public class Eva {
      */
     private GENDER gender;
 
+    private String password;
+
+    public boolean loginTo(String password) {
+        return this.password.equals(password);
+    }; 
+
+    public String getId() {
+        return id;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public GENDER getGender() {
+        return gender;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/Application/VVSP.java
+++ src/Application/VVSP.java
@@ -1,8 +1,10 @@
 package Application;
 
-import java.io.*;
+import java.time.LocalDate;
 import java.util.*;
 import Adamdar.Student;
+import Enums.GENDER;
+import Adamdar.Admin;
 
 /**
  * @author Meiramkhan Alinur
@@ -21,7 +23,8 @@ public class VVSP {
     public List<Student> spisok;
 
     public void executeVVSP() {
-        new Window().run();;
+        new Window().run();
+
     }
 
 }
@@ -38,13 +41,14 @@ class Window {
             switch (choice) {
                 case "1":
                     System.out.println("-> selected: LOGIN");
+                    printLogin();
                     break;
                 case "67":
                     isRunning = false;
                     System.out.println("Goodbye lol...");
                     break;
                 default:
-                    System.out.println("ERROR: wrong input stoopid");
+                    Printer.printError("ERROR: wrong input stoopid");
             }
         }
     }
@@ -53,6 +57,67 @@ class Window {
         System.out.println("\n--- VVSP SYSTEM MENU ---");
         System.out.println("1: LOGIN");
         System.out.println("67: EXIT");
-        System.out.print("Выберите действие: ");
+        System.out.print("Choose action: ");
     }
-}
+
+    private void printLogin() {
+        boolean isRunning = true;
+
+        List<Admin> admins = DataStorage.loadAdmins();
+
+        if (admins.isEmpty()) {
+            LocalDate localDate = LocalDate.parse("1970-01-01");
+            Admin firstborn = new Admin("0", "fenya", "fenya@kbtu.kz", localDate, GENDER.CROISSANT, "1234");
+            admins.add(firstborn);
+            DataStorage.saveAdmins(admins);
+        }
+
+        while (isRunning) {
+            System.out.println("--- VVSP LOGIN PAGE ---");
+            System.out.println("login: ");
+            String login = scanner.nextLine();
+            System.out.println("password: ");
+            String password = scanner.nextLine();
+
+            for (Admin eva : admins) {
+                if (eva.getName().equals(login)) {
+                    if (eva.loginTo(password)) {
+                        isRunning = false;
+                        Printer.printSucces("Login succes !");
+                        printAdminPanel();
+                    }
+                }
+            }
+
+            Printer.printError("Login or password incorrect");
+        }
+
+    }
+
+    private void printAdminPanel() {
+        boolean isRunning = true;
+        int choice = 0;
+
+        while (isRunning) {
+            System.out.println("--- ADMIN PANEL ---");
+            System.out.println("1: Create User");
+            System.out.println("2: Edit User");
+            System.out.println("3: Delete User");
+            System.out.println("67: EXIT");
+
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("-> selected: LOGIN");
+                    break;
+                case 67:
+                    isRunning = false;
+                    System.out.println("Goodbye lol...");
+                    break;
+                default:
+                    Printer.printError("ERROR: wrong input stoopid");
+            }
+        }
+    }
+}       
