3d7f66e048ad041c3bc0cffbc26dc057438658c3
Alaa-113 <alaaalazab87@gmail.com>
2026-04-25 20:13:57 +0500

    Added required research  with comparator and exception

--- src/Research/ResearchDELO.java
+++ src/Research/ResearchDELO.java
@@ -1,17 +1,45 @@
+// the project requires to print the resaerch paper sof all the resaercher s in the university(they have to be sorted by date|| citation||length of research )
+
+
 package Research;
 
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
-/**
- * @author Meiramkhan Alinur
- */
 public class ResearchDELO {
 
-    /**
-     * Default constructor
-     */
-    public ResearchDELO() {
+    public static void printAllPapers(List<Researcher>Allresearchers, Comparator<ResearchPaper>comparator){
+        List<ResearchPaper> Allpapers=new ArrayList<>();
+
+
+        for (Researcher researcher:Allresearchers){
+            Allpapers.addAll(researcher.getResearchPapers());
+
+        }
+        Allpapers.sort(comparator);
+        System.out.println("this researcher has "+Allpapers.size()+ "papers");
+
+        for(ResearchPaper paper:Allpapers){
+            System.out.println(paper);
+        }
+    }
+    
+
+
+
+    public static Researcher getTopCited(List<Researcher> researchers){
+        if(researchers==null || researchers.isEmpty())return null;
+        Researcher Topcited=researchers.get(0);
+        for(Researcher r:researchers  ){
+            if(r.getTotalcitations()>Topcited.getTotalcitations()){
+                Topcited=r;
+
+            }
+
+        }
+        return Topcited;
+
     }
 
 }
