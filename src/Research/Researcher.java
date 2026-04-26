3e391dc5469a38d2f5f171b0cd49581529358570
Alaa-113 <alaaalazab87@gmail.com>
2026-04-26 18:10:31 +0500

    chnages

--- src/Research/Researcher.java
+++ src/Research/Researcher.java
@@ -7,75 +7,8 @@
 
 package Research;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 public interface Researcher {
-
-    List<ResearchPaper> getResearchPapers();
-    List<ResearchProject>getResearchProjects();
-
-    void setResearchPapers(List<ResearchPaper> papers);
-    void setResearchProjects(List<ResearchProject> projects);
-
-
-   default void addResearchPaper(ResearchPaper paper){
-        getResearchPapers().add(paper);};
-
-    default void addResearchProject(ResearchProject project){
-        getResearchProjects().add(project);
-
-    };
-
-
-    default int getHindex(){
-        List<Integer>citations=new ArrayList<>();
-
-        for(ResearchPaper paper: getResearchPapers()){
-            citations.add(paper.getCitations());
-        }
-
-
-        citations.sort(Collections.reverseOrder());
-
- 
-    int h=0;
-    for(int i=0; i<citations.size();i++){
-        if(citations.get(i)>=i+1){
-            h=i+1;
-        }else{break;}
-        
-    }return h;   }
-
-
-
-    default int getTotalcitations(){
-        int total=0;
-        for(ResearchPaper paper: getResearchPapers()){
-            total+=paper.getCitations();
-        }
-        return total;
-    }
-
-    default int getPapercount(){
-        return getResearchPapers().size();
-
-    }
-
-
-
-    default void printpapers(Comparator <ResearchPaper> comparator){
-    List<ResearchPaper>sorted=new ArrayList<>(getResearchPapers());
-    sorted.sort(comparator);
-    System.out.println("we have "+getPapercount()+ "papers");
-    for(ResearchPaper paper:sorted){
-        System.out.println(paper);
-    }
-
-}
-
-
+    
+    ResearchDELO getResearchDELO();
     
 }
